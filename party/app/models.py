bd634ba6a2e0d269263df8cfd7d53855691a458d
rerekiis <deskyman@mail.ru>
2025-03-16 16:13:45 +0500

    были созданы модели

--- party/app/models.py
+++ party/app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+    name = models.CharField(max_length=32)
+    wallet = models.IntegerField()
+    invited = models.BooleanField(default=False)
+    party = models.ForeignKey('Party', on_delete=models.CASCADE)
+    
+
+    
+class Party(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    people = models.IntegerField()
+    date = models.DateField()
+    location = models.CharField(max_length=64)
+    image = models.ImageField(upload_to='party_images/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+ 
--- party/app/views.py
+++ party/app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Party
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
